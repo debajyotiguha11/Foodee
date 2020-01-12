06550465d8a0e502b63dcd877aaf2114ead4613b
Debjyoti Guha <debajyotiguha11@gmail.com>
2020-01-12 18:44:52 +0530

    Update footer.php

--- restaurant/includes/footer.php
+++ restaurant/includes/footer.php
@@ -2,5 +2,6 @@
 
 </div>
 </div>
+<div class="footer"><small>&copy; Copyright <?php echo date("Y");?>, Debjyoti Guha</small> <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" />
 </body>
 </html>
