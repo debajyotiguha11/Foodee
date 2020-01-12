4168303c17197950852ebf54118a89a27cea12d6
Debjyoti Guha <debajyotiguha11@gmail.com>
2020-01-12 17:59:42 +0530

    Update connect.php

--- connection/connect.php
+++ connection/connect.php
@@ -1,10 +1,11 @@
 <?php
+date_default_timezone_set("Asia/Kolkata");
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '12345');
-define('DB_NAME', 'food');
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'y8IYlrRFzu');
+define('DB_PASSWORD', 'vLXJDtyjxw');
+define('DB_NAME', 'y8IYlrRFzu');
  
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
