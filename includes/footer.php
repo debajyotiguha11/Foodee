2fa3a6f30092a4d657ac2b60912533e16d4e6002
Debjyoti Guha <debajyotiguha11@gmail.com>
2020-01-12 18:44:26 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,5 +4,6 @@
 </div>
 <script src="js/script.js">
 </script>
+<div class="footer"><small>&copy; Copyright <?php echo date("Y");?>, Debjyoti Guha</small> <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png" />
 </body>
 </html>
